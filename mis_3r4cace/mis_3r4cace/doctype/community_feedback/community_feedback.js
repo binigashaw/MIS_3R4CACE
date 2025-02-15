b2eacfc8c7fd12ab2ae535f5677b3c5005aa62ad
Binyam <binigashaw@gmail.com>
2025-02-15 14:11:49 +0300

    community Feedback and other customization

---
+++ mis_3r4cace/mis_3r4cace/doctype/community_feedback/community_feedback.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Binyam Abebaw and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Community Feedback", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ mis_3r4cace/mis_3r4cace/doctype/grievance_report/grievance_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Binyam Abebaw and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class GrievanceReport(Document):
+	pass
