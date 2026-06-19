a0c72a811b51577b25e9ff4a8ca5f74cbb83695a
Binyam <binigashaw@gmail.com>
2026-06-19 13:47:17 +0300

    update left over subproject and vsla member

---
+++ mis_3r4cace/mis_3r4cace/doctype/left_over_subproject/left_over_subproject.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Binyam Abebaw and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Left Over Subproject", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ mis_3r4cace/mis_3r4cace/doctype/left_over_subproject/test_left_over_subproject.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Binyam Abebaw and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeftOverSubproject(FrappeTestCase):
+	pass
